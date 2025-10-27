bb1242d369ca7265523f3d081b1fd4ed8d28daa1
Reosta Bayu Pratama Pane <reosta.pratama10@gmail.com>
2025-10-27 13:41:54 +0700

    template view

---
+++ resources/views/layouts/components/style.blade.php
@@ -0,0 +1,13 @@
+    
+    <!-- Node Waves CSS -->
+    <link href="{{ asset('assets/plugin/node-waves/waves.min.css') }}" rel="stylesheet"> 
+
+    <!-- Choices CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/plugin/choices-js/public/assets/styles/choices.min.css') }}">
+
+    <!-- Auto Complete CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/plugin/@tarekraafat/autocomplete.js/css/autoComplete.css') }}">
+
+    <!-- Simplebar CSS -->
+    <link href="{{ asset('assets/plugin/simplebar/simplebar.min.css') }}" rel="stylesheet" >
+
