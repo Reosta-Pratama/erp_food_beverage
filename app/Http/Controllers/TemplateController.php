351907e545e9a8509d871902063484eb5d69e639
Reosta Bayu Pratama Pane <reosta.pratama10@gmail.com>
2025-10-28 11:37:03 +0700

    template view

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -10,4 +10,76 @@ class TemplateController extends Controller
     public function alerts(){
         return view('template.basic-ui.alerts');
     }
+
+    public function badge(){
+        return view('template.basic-ui.badge');
+    }
+
+    public function breadcrumb(){
+        return view('template.basic-ui.breadcrumb');
+    }
+    
+    public function buttonGroup(){
+        return view('template.basic-ui.button-group');
+    }
+
+    public function buttons(){
+        return view('template.basic-ui.buttons');
+    }
+
+    public function cards(){
+        return view('template.basic-ui.cards');
+    }
+
+    public function dropdowns(){
+        return view('template.basic-ui.dropdowns');
+    }
+
+    public function imageFigures(){
+        return view('template.basic-ui.image-figures');
+    }
+
+    public function linkInteraction(){
+        return view('template.basic-ui.link-interactions');
+    }
+
+    public function listGroup(){
+        return view('template.basic-ui.list-group');
+    }
+
+    public function navs(){
+        return view('template.basic-ui.navs');
+    }
+
+    public function objectFit(){
+        return view('template.basic-ui.object-fit');
+    }
+
+    public function pagination(){
+        return view('template.basic-ui.pagination');
+    }
+
+    public function popovers(){
+        return view('template.basic-ui.popovers');
+    }
+
+    public function progress(){
+        return view('template.basic-ui.progress');
+    }
+
+    public function spinners(){
+        return view('template.basic-ui.spinners');
+    }
+
+    public function toasts(){
+        return view('template.basic-ui.toasts');
+    }
+
+    public function tooltips(){
+        return view('template.basic-ui.tooltips');
+    }
+
+    public function typography(){
+        return view('template.basic-ui.typography');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,24 @@ Route::prefix('template')
     ->group(function () {
         // Basic
         Route::get('/alerts', [TemplateController::class, 'alerts'])->name('alerts');
+        Route::get('/badge', [TemplateController::class, 'badge'])->name('badge');
+        Route::get('/breadcrumb', [TemplateController::class, 'breadcrumb'])->name('breadcrumb');
+        Route::get('/buttonGroup', [TemplateController::class, 'buttonGroup'])->name('buttonGroup');
+        Route::get('/buttons', [TemplateController::class, 'buttons'])->name('buttons');
+        Route::get('/cards', [TemplateController::class, 'cards'])->name('cards');
+        Route::get('/dropdowns', [TemplateController::class, 'dropdowns'])->name('dropdowns');
+        Route::get('/imageFigures', [TemplateController::class, 'imageFigures'])->name('imageFigures');
+        Route::get('/linkInteraction', [TemplateController::class, 'linkInteraction'])->name('linkInteraction');
+        Route::get('/listGroup', [TemplateController::class, 'listGroup'])->name('listGroup');
+        Route::get('/navs', [TemplateController::class, 'navs'])->name('navs');
+        Route::get('/objectFit', [TemplateController::class, 'objectFit'])->name('objectFit');
+        Route::get('/pagination', [TemplateController::class, 'pagination'])->name('pagination');
+        Route::get('/popovers', [TemplateController::class, 'popovers'])->name('popovers');
+        Route::get('/progress', [TemplateController::class, 'progress'])->name('progress');
+        Route::get('/spinners', [TemplateController::class, 'spinners'])->name('spinners');
+        Route::get('/toasts', [TemplateController::class, 'toasts'])->name('toasts');
+        Route::get('/tooltips', [TemplateController::class, 'tooltips'])->name('tooltips');
+        Route::get('/typography', [TemplateController::class, 'typography'])->name('typography');
 
         // Advanced
 
