81bb861016d704206fed6e490caac336e13af85d
Reosta Bayu Pratama Pane <reosta.pratama10@gmail.com>
2025-12-04 11:24:44 +0700

    Sprint 7 - QC

--- routes/sprint/sprint_07_quality_control.php
+++ routes/sprint/sprint_07_quality_control.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Inventory\QualityControlController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,17 +23,17 @@ Route::middleware(['role:admin,operator', 'permission:quality_control.manage'])
     ->prefix('quality-control/inspections')
     ->name('quality-control.inspections.')
     ->group(function () {
-        // Route::get('/', [QualityControlController::class, 'index'])->name('index');
-        // Route::get('/create', [QualityControlController::class, 'create'])->name('create');
-        // Route::post('/', [QualityControlController::class, 'store'])->name('store');
-        // Route::get('/export', [QualityControlController::class, 'export'])->name('export');
-        // Route::get('/{qcCode}', [QualityControlController::class, 'show'])->name('show');
-        // Route::get('/{qcCode}/edit', [QualityControlController::class, 'edit'])->name('edit');
-        // Route::put('/{qcCode}', [QualityControlController::class, 'update'])->name('update');
-        // Route::delete('/{qcCode}', [QualityControlController::class, 'destroy'])->name('destroy');
-        // Route::get('/{qcCode}/print', [QualityControlController::class, 'print'])->name('print');
+        Route::get('/', [QualityControlController::class, 'index'])->name('index');
+        Route::get('/create', [QualityControlController::class, 'create'])->name('create');
+        Route::post('/', [QualityControlController::class, 'store'])->name('store');
+        Route::get('/export', [QualityControlController::class, 'export'])->name('export');
+        Route::get('/{qcCode}', [QualityControlController::class, 'show'])->name('show');
+        Route::get('/{qcCode}/edit', [QualityControlController::class, 'edit'])->name('edit');
+        Route::put('/{qcCode}', [QualityControlController::class, 'update'])->name('update');
+        Route::delete('/{qcCode}', [QualityControlController::class, 'destroy'])->name('destroy');
+        Route::get('/{qcCode}/print', [QualityControlController::class, 'print'])->name('print');
         
         // // Batch Approval/Rejection Actions
-        // Route::post('/{qcCode}/approve-batch', [QualityControlController::class, 'approveBatch'])->name('approve-batch');
-        // Route::post('/{qcCode}/reject-batch', [QualityControlController::class, 'rejectBatch'])->name('reject-batch');
+        Route::post('/{qcCode}/approve-batch', [QualityControlController::class, 'approveBatch'])->name('approve-batch');
+        Route::post('/{qcCode}/reject-batch', [QualityControlController::class, 'rejectBatch'])->name('reject-batch');
     });
